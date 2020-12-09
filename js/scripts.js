53be11af006cf65abaf350719f78ca6717ff7298
Crue Jameson <ericcjameson@gmail.com>
2020-12-08 17:07:50 -0700

    basic math JS manipulation

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var myName = 'Cruebee';
+document.write(myName + '<br>');
+
+var simpleAdd = 2 + 2;
+document.write(simpleAdd + '<br>');
+
+var size = 100;
+var double = size * 2;
+document.write(double + '<br>');
+
+var number = 100;
+var half = number / 2;
+document.write(half + '<br>');
+
+var value = 100;
+var doubleVal = value * 2;
+var minVal = doubleVal * 2 - value / 2;
+document.write(minVal);
